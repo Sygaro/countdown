1db94b64984b213d1264e973106d21cb8dd2e8c5
Sygaro <reidarg@gmail.com>
2025-09-19 22:16:56 +0200

    opprydding

---
+++ tools/get_raw_links.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER="Sygaro"
+REPO="countdown"
+BRANCH="fix/fix_18sept"
+
+# Sett default MAPPE hvis ingen argumenter
+MAPPE=${1:-""}
+
+# Hent filstruktur fra GitHub API
+curl -s "https://api.github.com/repos/$USER/$REPO/git/trees/$BRANCH?recursive=1" \
+  | jq -r --arg MAPPE "$MAPPE" '
+      .tree[]
+      | select(.type=="blob")
+      | select($MAPPE == "" or (.path | startswith($MAPPE)))
+      | .path
+    ' \
+  | while read path; do
+      echo "https://raw.githubusercontent.com/$USER/$REPO/$BRANCH/$path"
+    done
